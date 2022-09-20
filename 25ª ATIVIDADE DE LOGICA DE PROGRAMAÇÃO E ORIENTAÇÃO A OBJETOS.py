b6997896bb9441495b45b207c566c65ef4b0dc45
Bruno Lopes <107210759+BrunochxiyckaoumLopes@users.noreply.github.com>
2022-09-20 19:54:44 -0300

    Add files via upload

---
+++ 25ª ATIVIDADE DE LOGICA DE PROGRAMAÇÃO E ORIENTAÇÃO A OBJETOS.py
@@ -0,0 +1,19 @@
+#Inverter a ordem da string
+
+frase = "etsedron od roiam o é tropS O"
+print(frase[::-1])
+
+#Acessar caracteres específicos de uma string
+
+palavra = "pipoca" 
+print(palavra[0])
+print(palavra[1])
+print(palavra[2])
+print(palavra[3])
+print(palavra[4])
+print(palavra[5])
+
+#Quantidade dos caracteres
+
+palavra2 = "Otorrinolaringologista"
+print(len(palavra2))
